a7444c4764be97479575106fc835afe5cecd3b30
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2023-07-15 20:28:12 +0800

    tests: improve test for succeses and errors

--- spec/qoi_spec.cr
+++ spec/qoi_spec.cr
@@ -1,6 +1,16 @@
 require "./spec_helper"
 describe Qoi do
-  it "is okay" do
-    Qoi::QOI_MAGIC.should eq 0x716f6966
+  describe "#errors" do
+    it "errors with zero width or/and height" do
+      expect_raises(Qoi::QOIHeader::QOIError, "invalid width in header, got 0") do
+        Qoi::QOIHeader.new(0_u32, 0_u32, Qoi::QOIChannel::QOI_RGBA, Qoi::QOIColorSpace::QOI_SRGB)
+      end
+    end
+  end
+  describe "#successes" do
+    it "is succesful with correct params" do
+      Qoi::QOI_MAGIC.should eq 0x716f6966
+      header = Qoi::QOIHeader.new(1_u32, 1_u32, Qoi::QOIChannel::QOI_RGBA, Qoi::QOIColorSpace::QOI_SRGB)
+    end
   end
 end
